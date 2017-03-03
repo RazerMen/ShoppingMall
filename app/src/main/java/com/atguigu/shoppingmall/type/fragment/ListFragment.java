945384fc9bf1ded0563a137ec13b3edb949c0a12
RazerMen <823903729@qq.com>
2017-03-03 15:18:59 +0800

    25、设置ListView的适配器和点击事件

--- app/src/main/java/com/atguigu/shoppingmall/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall/type/fragment/ListFragment.java
@@ -1,11 +1,16 @@
 package com.atguigu.shoppingmall.type.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.base.BaseFragment;
+import com.atguigu.shoppingmall.type.adapter.TypeLeftAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by 一名程序员 on 2017/2/22.
@@ -15,20 +20,43 @@ import com.atguigu.shoppingmall.base.BaseFragment;
 
 public class ListFragment extends BaseFragment {
 
-    private TextView textView;
+
+    @BindView(R.id.lv_left)
+    ListView lvLeft;
+    @BindView(R.id.rv_right)
+    RecyclerView rvRight;
+
+    //网络请求得到数据
+    private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
+            "办公文具", "数码周边", "游戏专区"};
+
+    private TypeLeftAdapter leftAdapter;
 
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setTextColor(Color.BLUE);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_list, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("aaaaa");
+
+        //设置适配器
+        leftAdapter = new TypeLeftAdapter(mContext,titles);
+        lvLeft.setAdapter(leftAdapter);
+        //设置item点击事件
+        lvLeft.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(mContext, "position" + position, Toast.LENGTH_SHORT).show();
+
+                //传入被点击的位置
+                leftAdapter.changeSelected(position);
+                //刷新适配器
+                leftAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
