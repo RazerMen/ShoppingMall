ce2deeee816ca8f9f54c40d574ec8f394c40fcba
RazerMen <823903729@qq.com>
2017-03-03 15:00:00 +0800

    24、实现ListFragment和TagFragment进行来回切换

---
+++ app/src/main/java/com/atguigu/shoppingmall/type/fragment/TagFragment.java
@@ -0,0 +1,34 @@
+package com.atguigu.shoppingmall.type.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.shoppingmall.base.BaseFragment;
+
+/**
+ * Created by 一名程序员 on 2017/2/22.
+ * <p>
+ * 作用：
+ */
+
+public class TagFragment extends BaseFragment {
+
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.BLUE);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("bbbbbbbb");
+    }
+}
--- app/src/main/java/com/atguigu/shoppingmall/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall/type/fragment/TypeFragment.java
@@ -1,5 +1,7 @@
 package com.atguigu.shoppingmall.type.fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -7,8 +9,11 @@ import android.widget.Toast;
 
 import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.base.BaseFragment;
+import com.atguigu.shoppingmall.main.MainActivity;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,7 +35,11 @@ public class TypeFragment extends BaseFragment {
     @BindView(R.id.fl_type)
     FrameLayout flType;
 
-    private String[] titles = {"分类","标签"};
+    private String[] titles = {"分类", "标签"};
+
+    private ArrayList<BaseFragment> fragments;
+
+    private Fragment tempFragment;
 
     @Override
     public View initView() {
@@ -43,12 +52,28 @@ public class TypeFragment extends BaseFragment {
     public void initData() {
         super.initData();
 
+        initListener();
+
+        initFragment();
+
+        switchFragment(fragments.get(0));
+    }
+
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new ListFragment());
+        fragments.add(new TagFragment());
+    }
+
+    private void initListener() {
         tl1.setTabData(titles);
 
         tl1.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
-                Toast.makeText(mContext, "position" + position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "position" + position, Toast.LENGTH_SHORT).show();
+                switchFragment(fragments.get(position));
+
             }
 
             @Override
@@ -58,7 +83,29 @@ public class TypeFragment extends BaseFragment {
         });
     }
 
+    private void switchFragment(Fragment currentFragment) {
+
+        if (tempFragment != currentFragment) {
+            MainActivity mainActivity = (MainActivity) mContext;
+            FragmentTransaction ft = mainActivity.getSupportFragmentManager().beginTransaction();
+            if (!currentFragment.isAdded()) {
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.add(R.id.fl_type, currentFragment);
+            } else {
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.show(currentFragment);
+            }
+            ft.commit();
+            tempFragment = currentFragment;
+        }
+    }
+
     @OnClick(R.id.iv_type_search)
     public void onClick() {
+        Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
     }
 }
