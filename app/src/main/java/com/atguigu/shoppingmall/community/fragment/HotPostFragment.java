4bb2df080048e61b64c3ff3d3d016f0e346d5654
RazerMen <823903729@qq.com>
2017-03-04 18:34:13 +0800

    34、设置适配器HotPostListViewAdapter

--- app/src/main/java/com/atguigu/shoppingmall/community/fragment/HotPostFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall/community/fragment/HotPostFragment.java
@@ -1,11 +1,23 @@
 package com.atguigu.shoppingmall.community.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.shoppingmall.R;
 import com.atguigu.shoppingmall.base.BaseFragment;
+import com.atguigu.shoppingmall.community.adapter.HotPostListViewAdapter;
+import com.atguigu.shoppingmall.community.bean.HotPostBean;
+import com.atguigu.shoppingmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by 一名程序员 on 2017/2/22.
@@ -15,20 +27,49 @@ import com.atguigu.shoppingmall.base.BaseFragment;
 
 public class HotPostFragment extends BaseFragment {
 
-    private TextView textView;
+
+    @BindView(R.id.lv_hot_post)
+    ListView lvHotPost;
+
+    private HotPostListViewAdapter adapter;
 
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setTextColor(Color.BLUE);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_hot_post, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("热帖");
+
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        OkHttpUtils.get().url(Constants.HOT_POST_URL).id(100).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.e("TAG", "联网失败==" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                Log.e("TAG", "联网成功==" + response);
+                processData(response);
+            }
+        });
+    }
+
+    private void processData(String json) {
+        HotPostBean hotPostBean = JSON.parseObject(json, HotPostBean.class);
+//        Toast.makeText(mContext, "" + hotPostBean.getResult().get(0).getSaying(), Toast.LENGTH_SHORT).show();
+        List<HotPostBean.ResultBean> result = hotPostBean.getResult();
+        if(result != null && result.size() > 0) {
+            //设置适配器
+            adapter = new HotPostListViewAdapter(mContext,result);
+            lvHotPost.setAdapter(adapter);
+        }
     }
 }
